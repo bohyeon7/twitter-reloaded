392233258b602fa28b88e29d86245c89cc6f0245
nana <nana@tyrelldev.space>
2024-09-22 22:58:04 +0900

    GlobalStyles 추가하기, layout 제외되는 라우터 생성

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "./components/layout";
 import Home from "./routes/home";
 import Profile from "./routes/profile";
+import Login from "./routes/login";
+import CreateAccount from "./routes/create-accout";
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
 
 const router = createBrowserRouter([
   {
@@ -18,13 +22,34 @@ const router = createBrowserRouter([
         element: <Profile />,
       },
     ],
-  }
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/create-account",
+    element: <CreateAccount />,
+  },
 ]);
+
+const GlobalStyles = createGlobalStyle`
+  ${reset};
+  * {
+  box-sizing: border-box;
+  }
+  body {
+    background-color: black;
+    color: white;
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+`;
 
 function App() {
 
   return (
     <>
+      <GlobalStyles />
       <RouterProvider router={router} />
     </>
   )
