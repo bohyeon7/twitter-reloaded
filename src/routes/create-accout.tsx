1bfbbf7007b5c929b484f63460a989f64f65ae37
nana <nana@tyrelldev.space>
2024-09-23 00:28:07 +0900

    계정생성 form 추가

--- src/routes/create-accout.tsx
+++ src/routes/create-accout.tsx
@@ -1,7 +1,62 @@
+import React, { useState } from "react";
+import styled from "styled-components"
+
+const Wrapper = styled.div``;
+
+const Form = styled.form``;
+
+const Input = styled.input``;
+
 export default function CreateAccount() {
+  const [isLoading, setLoading] = useState(false);
+  const [name, setName] = useState("");
+  const [email, setEamil] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { target: { name, value } } = e;
+    if (name === "name") {
+      setName(value);
+    } else if (name === "email") {
+      setEamil(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(name, email, password);
+
+  }
+
   return (
-    <>
-      <h1>CreateAccount</h1>
-    </>
+    <Wrapper>
+      <Form onSubmit={onSubmit}>
+        <Input
+          onChange={onChange}
+          name="name"
+          value={name}
+          placeholder="Name"
+          type="text"
+          required />
+        <Input
+          onChange={onChange}
+          name="email"
+          value={email}
+          placeholder="Email"
+          type="email"
+          required />
+        <Input
+          onChange={onChange}
+          name="password"
+          value={password}
+          placeholder="Password"
+          type="password"
+          required
+        />
+        <Input type="submit" value="Create Account" />
+      </Form>
+    </Wrapper>
   )
 }
