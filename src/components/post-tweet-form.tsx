6c7739924065d82e35a2555c484d5edad9bfa85e
nana <nana@tyrelldev.space>
2024-09-28 14:29:19 +0900

    [게시물] 파일업로드, query 로 가져온데이터 뿌리기

--- src/components/post-tweet-form.tsx
+++ src/components/post-tweet-form.tsx
@@ -1,7 +1,8 @@
-import { addDoc, collection, CollectionReference } from "firebase/firestore";
+import { addDoc, collection, CollectionReference, updateDoc } from "firebase/firestore";
 import { useState } from "react";
 import styled from "styled-components"
-import { auth, db } from "../firebase";
+import { auth, db, storage } from "../firebase";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 
 const Form = styled.form`
   display: flex;
@@ -80,12 +81,26 @@ export default function PostTweetForm() {
     if (!user || isLoading || tweet === "" || tweet.length > 180) return;
     try {
       setLoading(true);
-      await addDoc(collection(db, "tweets"), {
+      // tweet 업로드
+      const doc = await addDoc(collection(db, "tweets"), {
         tweet,
         createdAt: Date.now(),
         username: user.displayName || "Anonymous",
         userId: user.uid,
       });
+      // file 업로드
+      if (file) {
+        const locationRef = ref(
+          storage,
+          `tweets/${user.uid}-${user.displayName}/${doc.id}`);
+        const result = await uploadBytes(locationRef, file); // 파일업로드
+        const url = await getDownloadURL(result.ref); // 업로드한 파일 경로
+        await updateDoc(doc, {
+          photo: url
+        });
+      }
+      setTweet("");
+      setFile(null);
     } catch (error) {
       console.log(error);
     } finally {
@@ -95,6 +110,7 @@ export default function PostTweetForm() {
 
   return <Form onSubmit={onSubmit}>
     <TextArea
+      required
       rows={5}
       maxLength={180}
       value={tweet}
