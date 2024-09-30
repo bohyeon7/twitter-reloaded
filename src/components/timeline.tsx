7ebdaff5deab9b5c92f882d88f524e154843cc12
nana <qhgus4856@gmail.com>
2024-09-30 20:14:37 +0900

    [게시물] 실시간 db 반영 onSnapshot, unsubscribe 추가

--- src/components/timeline.tsx
+++ src/components/timeline.tsx
@@ -1,4 +1,4 @@
-import { collection, getDocs, orderBy, query } from "firebase/firestore";
+import { collection, getDocs, onSnapshot, orderBy, query, Unsubscribe } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import styled from "styled-components";
 import { db } from "../firebase";
@@ -13,32 +13,40 @@ export interface ITweet {
   createdAt: number;
 }
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  margin-top: 60px;
+`;
 
 export default function Timeline() {
   // interface 배열 초기값
   const [tweets, setTweets] = useState<ITweet[]>([]);
-  const fetchTweets = async () => {
-    const tweetsQuery = query(
-      collection(db, "tweets"),
-      orderBy("createdAt", "desc")
-    );
-    const snapshot = await getDocs(tweetsQuery);
-    const tweets = snapshot.docs.map((doc) => {
-      const { photo, tweet, userId, username, createdAt } = doc.data();
-      return {
-        photo,
-        tweet,
-        userId,
-        username,
-        createdAt,
-        id: doc.id,
-      }
-    });
-    setTweets(tweets);
-  };
+
   useEffect(() => {
+    let unsubscribe: Unsubscribe | null = null;
+    const fetchTweets = async () => {
+      const tweetsQuery = query(
+        collection(db, "tweets"),
+        orderBy("createdAt", "desc")
+      );
+      unsubscribe = await onSnapshot(tweetsQuery, (snapshot) => {
+        const tweets = snapshot.docs.map((doc) => {
+          const { photo, tweet, userId, username, createdAt } = doc.data();
+          return {
+            photo,
+            tweet,
+            userId,
+            username,
+            createdAt,
+            id: doc.id,
+          }
+        });
+        setTweets(tweets);
+      });
+    };
     fetchTweets();
+    return () => {
+      unsubscribe && unsubscribe();
+    }
   }, []);
   return <Wrapper>
     {tweets.map((tweet) => (
